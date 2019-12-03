57e3a3b10f73900f3b7851a0135e0b831e2a955b
Leon2217 <46972973+Leon2217@users.noreply.github.com>
2019-12-03 10:10:54 -0300

    Corrigindo erro onde não era mostrado a soma de apostas no InicialCaixa

--- Caixa/Classes/Conexao.cs
+++ Caixa/Classes/Conexao.cs
@@ -24,11 +24,11 @@ namespace Caixa
             ////Padaria Vista Verde
             //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "25.36.55.44", "root", "Coxinha#2019");
             ////Padaria Parque das Américas 
-            //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "25.48.37.81", "root", "Coxinha#2019");
+            string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "25.48.37.81", "root", "Coxinha#2019");
             //Padrão Padaria
             //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "127.0.0.1", "root", "Coxinha#2019");
             //Padrao Estagiario01
-            string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "127.0.0.1", "root", "");
+            //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "127.0.0.1", "root", "");
             //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql;sslMode=none;pooling=false", "127.0.0.1", "root", "ALUNOS");
             //string configuracao = string.Format("server={0};user id={1}; password={2};database=mysql; pooling=false", "127.0.0.1", "root", "aluno@etep");
             // tenta estabelecer conectar
